5b02f7aac252c419470fec6540f73e6d32908c07
Junha <junha@Junhaui-MacBook-Pro.local>
2017-11-04 01:59:12 +0900

    config serializer error, made allimages api(setting url)

--- nomadgram/images/serializers.py
+++ nomadgram/images/serializers.py
@@ -2,19 +2,19 @@ from rest_framework import serializers
 from . import models
 # serializer -> Framework와 Python world의 다리
 
-class ImageSerializer(serializers.Serializer):
+class ImageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Image
         fields = '__all__'
 
-class CommentSerializer(serializers.Serializer):
+class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Comment
         fields = '__all__'
 
-class LikeSerializer(serializers.Serializer):
+class LikeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Like
